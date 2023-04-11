c367846f108dc1a5a175db1d053385bea3e6400e
chetanknightstar <126722416+chetanknightstar@users.noreply.github.com>
2023-04-12 03:56:49 +0530

    for terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 
-resource "aws_instance" "web" {
+resource "aws_instance" "web1" {
   #ami           = "ami-064087b8d355e9051"
   #instance_type = "t2.micro"
   ami            = var.ami
@@ -11,7 +11,7 @@ resource "aws_instance" "web" {
 }
 
 
-resource "aws_instance" "web" {
+resource "aws_instance" "web2" {
   ami            = var.ami
   instance_type  = var.instance_type
 
