1cacb77db58561eac1b5503b7fad83d09509ca33
chetanknightstar <126722416+chetanknightstar@users.noreply.github.com>
2023-04-12 04:15:31 +0530

    for terraform

--- main.tf
+++ main.tf
@@ -10,12 +10,3 @@ resource "aws_instance" "web1" {
   }
 }
 
-
-resource "aws_instance" "web2" {
-    ami            = var.ami
-    instance_type  = var.instance_type
-
-    tags = {
-    Name = "instance02"
-  }
-}
