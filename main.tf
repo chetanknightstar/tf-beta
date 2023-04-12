84ae0c57e973a4ef7bfb6d56042a5d221adcdc9c
chetanknightstar <126722416+chetanknightstar@users.noreply.github.com>
2023-04-12 16:51:46 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,18 +1,20 @@
 resource "aws_instance" "web1" {
-  ami           = "ami-064087b8d355e9051"
-  instance_type = "t3.micro"
+  #ami           = "ami-064087b8d355e9051"
+  #instance_type = "t3.micro"
+  ami            = var.ami
+  instance_type  = var.instance_type
 
   tags = {
-    Name = "HelloWorld"
+    Name = "kartiki"
   }
 }
 
 resource "aws_instance" "web2" {
-  ami           = "ami-064087b8d355e9051"
-  instance_type = "t3.micro"
+  ami            = var.ami
+  instance_type  = var.instance_type
 
   tags = {
-    Name = "HelloWorld007"
+    Name = "amrapali"
   }
 }
 
